ec353e5267c167768ec0b1c84c243cb73646ac6f
Tadeo <tadeo@ravegan.com>
2020-05-12 19:25:16 -0300

    finales detector

---
+++ Assets/FinalBalance.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FinalBalance : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponentInParent<Player>())
+        {
+            FinaleCountHandler.Instance.balance = true;
+            SceneManager.LoadScene(0);
+        }
+    }
+}
