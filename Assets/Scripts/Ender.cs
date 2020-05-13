0d5a6b6b434e63ac45e696ec0f266aae39467666
Sehgel <lucasemanuelgonzalez8@gmail.com>
2020-05-12 22:14:30 -0300

    Motion blur remove

---
+++ Assets/Scripts/Ender.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Ender : MonoBehaviour
+{
+    public int scene_id = 0;
+
+    private void OnCollisionEnter(Collision other)
+    {
+        print(other.gameObject.tag);
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        print(other.tag);
+        if (other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(scene_id);
+        }
+    }
+}
