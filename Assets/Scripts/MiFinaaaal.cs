cf51b36e9d6a7fb777a65e76216c596edd151385
VG <vengadoravg@gmail.com>
2020-05-12 21:45:40 -0400

    final

---
+++ Assets/Scripts/MiFinaaaal.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MiFinaaaal : MonoBehaviour {
+  public int que;
+  public bool t = false;
+
+  void OnTriggerEnter(Collider c) {
+    if (t) return;
+    if (c.GetComponentInParent<Player>()) {
+      t = true;
+      StartCoroutine(_Trigger());
+    }
+  }
+
+  IEnumerator _Trigger () {
+    yield return new WaitForSeconds(1);
+    switch (que) {
+      case 0: FinaleCountHandler.Instance.FinalMaldad(); break;
+      case 1: FinaleCountHandler.Instance.FinalBalance(); break;
+      case 2: FinaleCountHandler.Instance.FinalMoralRota(); break;
+    }
+  }
+}
